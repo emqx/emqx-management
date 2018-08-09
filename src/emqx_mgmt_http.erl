2032a17f874dc55d5de5f09f67715e650a8723ac
turtled <turtled@emqtt.io>
2018-08-09 14:52:54 +0800

    Update start_http API

--- src/emqx_mgmt_http.erl
+++ src/emqx_mgmt_http.erl
@@ -37,15 +37,13 @@ stop_listeners() ->
     lists:foreach(fun stop_listener/1, listeners()).
 
 start_listener({Proto, Port, Options}) when Proto == http ->
-    Handlers = [minirest:map(Handler) || Handler <- http_handlers()],
     Dispatch = [{"/status", emqx_mgmt_http, []},
-                {"/api/v2/[...]", minirest, Handlers}],
+                {"/api/v2/[...]", minirest, http_handlers()}],
     minirest:start_http(listener_name(Proto), [{port, Port}] ++ Options, Dispatch);
 
 start_listener({Proto, Port, Options}) when Proto == https ->
-    Handlers = [minirest:map(Handler) || Handler <- http_handlers()],
     Dispatch = [{"/status", emqx_mgmt_http, []},
-                {"/api/v2/[...]", minirest, Handlers}],
+                {"/api/v2/[...]", minirest, http_handlers()}],
     minirest:start_https(listener_name(Proto), [{port, Port}] ++ Options, Dispatch).
 
 stop_listener({Proto, Port, _}) ->
