ebd906db490272c45a349d0104b9374ed14e0a3f
Neksus <neksus@foxmail.com>
2019-12-19 14:53:01 +0800

    add base64 support

--- src/emqx_mgmt_api_pubsub.erl
+++ src/emqx_mgmt_api_pubsub.erl
@@ -188,7 +188,9 @@ parse_subscribe_params(Params) ->
 parse_publish_params(Params) ->
     Topics   = topics(name, get_value(<<"topic">>, Params), get_value(<<"topics">>, Params, <<"">>)),
     ClientId = get_value(<<"clientid">>, Params),
-    Payload  = get_value(<<"payload">>, Params, <<>>),
+    Encoding  = get_value(<<"payload_encoding">>, Params, <<"string">>),
+    _Payload  = get_value(<<"payload">>, Params, <<>>),
+    Payload = decode_payload(_Payload,Encoding),
     Qos      = get_value(<<"qos">>, Params, 0),
     Retain   = get_value(<<"retain">>, Params, false),
     {ClientId, Topics, Qos, Retain, Payload}.
@@ -235,3 +237,6 @@ resp_topic(undefined, Topics) ->
     {topics, Topics};
 resp_topic(Topic, _) ->
     {topic, Topic}.
+
+decode_payload(Payload, <<"base64">>) -> base64:decode(Payload);
+decode_payload(Payload, _) -> Payload.
