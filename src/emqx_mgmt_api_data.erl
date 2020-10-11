4efadbce9b2107b8725b7078170131b372aba23a
turtleDeng <deng@emqx.io>
2020-10-11 13:18:26 +0800

    chore(data): Optimize import order

--- src/emqx_mgmt_api_data.erl
+++ src/emqx_mgmt_api_data.erl
@@ -159,12 +159,12 @@ import(_Bindings, Params) ->
                                 emqx_mgmt:import_blacklist(maps:get(<<"blacklist">>, Data, [])),
                                 emqx_mgmt:import_applications(maps:get(<<"apps">>, Data, [])),
                                 emqx_mgmt:import_users(maps:get(<<"users">>, Data, [])),
+                                emqx_mgmt:import_modules(maps:get(<<"modules">>, Data, [])),
                                 emqx_mgmt:import_auth_clientid(maps:get(<<"auth_clientid">>, Data, [])),
                                 emqx_mgmt:import_auth_username(maps:get(<<"auth_username">>, Data, [])),
                                 emqx_mgmt:import_auth_mnesia(maps:get(<<"auth_mnesia">>, Data, []), Version),
                                 emqx_mgmt:import_acl_mnesia(maps:get(<<"acl_mnesia">>, Data, []), Version),
                                 emqx_mgmt:import_schemas(maps:get(<<"schemas">>, Data, [])),
-                                emqx_mgmt:import_modules(maps:get(<<"modules">>, Data, [])),
                                 logger:debug("The emqx data has been imported successfully"),
                                 return()
                             catch Class:Reason:Stack ->
